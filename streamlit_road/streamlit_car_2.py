04e4ff8d03ff5dbf9791c7c34a6cefa4bfed2150
tiagoru <135262594+tiagoru@users.noreply.github.com>
2023-12-04 17:09:32 +0100

    Update streamlit_car_2.py
    include new figures for testing

--- streamlit_road/streamlit_car_2.py
+++ streamlit_road/streamlit_car_2.py
@@ -27,7 +27,7 @@ page=st.sidebar.radio("Go to", pages)
 if page == pages[0]: 
     st.write("### Project")
     st.write("#### Aim: optimize three classification models with the best overall performances for predicting severe accidents (at least one hospitalized or killed person in an accident versus only slightly or non-injured persons), and then calibrate, evaluate and interpret all three models")
-    st.write("##### authors: Johanna, Tiago Tobias ")
+    st.write("##### authors: Johanna, Tiago , Tobias ")
 
 if page == pages[1] :
     st.write("### Data description")
@@ -43,17 +43,31 @@ if page == pages[1] :
 
 # editing visualization of the data
 if page == pages[2] : 
-    st.write("### DataVizualization")
+    st.title("Data Vizualization")
+    st.write("### Accidents per departments")
+    
     image = Image.open('accidents_per_department.png')
     image_size = (1000, 400)
     st.image(image, width=image_size[0], caption='Accidents per Department')
-      
+    
+    st.write("### Data exploration")
+    
+    image = Image.open('exploratory_1.png')
+    image_size = (1000, 400)
+    st.image(image, width=image_size[0], caption='weather and road conditions')
+
+    st.write("### Data exploration")
+    
+    image = Image.open('exploratory_2.png')
+    image_size = (1000, 400)
+    st.image(image, width=image_size[0], caption='local and road conditions')  
+  
 
 # modeling results
 
 if page == pages[3] : 
-    st.write("### modelling")
-    st.write("### variables correlation")
+    st.write("### Modelling")
+    st.write("### Variables Correlation")
     image = Image.open('correlation.png')
     image_size = (1000, 400)
     st.image(image, width=image_size[0], caption='correlations')
@@ -63,3 +77,13 @@ if page == pages[3] :
         image = Image.open('Matrix_XG.png')
         #image_size = (1000, 400)
         st.image(image, width=image_size[0], caption='Confusion matrix of the XGboost model')
+    if st.checkbox("Show Decision tree model") :
+        st.write("### teste")
+        #image = Image.open('Matrix_XG.png')
+        #image_size = (1000, 400)
+        #st.image(image, width=image_size[0], caption='Confusion matrix of the XGboost model')
+    if st.checkbox("Show Random Forest model") :
+        st.write("### teste")
+        #image = Image.open('Matrix_XG.png')
+        #image_size = (1000, 400)
+        #st.image(image, width=image_size[0], caption='Confusion matrix of the XGboost model')
